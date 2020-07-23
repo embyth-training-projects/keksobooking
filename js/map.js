98cdf86ef40ffd37a5c4c961d3058773cc1b061d
Rostyslav Miniukov <thresh1337@gmail.com>
2020-07-23 14:46:22 +0300

    Adding form validation logic

--- js/map.js
+++ js/map.js
@@ -400,6 +400,7 @@ function disableMap() {
   toggleInputs(adForm, true);
   movePinToDefaultPosition();
   removeMapHandlers();
+  removeFormOfferHandlers();
   mapField.classList.add('map--faded');
   adForm.classList.add('ad-form--disabled');
 }
@@ -407,10 +408,151 @@ function disableMap() {
 // Активируем карту
 function enableMap() {
   addMapHandlers();
+  addFormOfferHandlers();
   toggleInputs(adForm, false);
   insertMapElements(generatedAds);
   mapField.classList.remove('map--faded');
   adForm.classList.remove('ad-form--disabled');
+}
+
+// ================================= //
+// ======== FORM VALIDATION ======== //
+// ================================= //
+
+var formOfferTitle = adForm.querySelector('#title');
+var formOfferPrice = adForm.querySelector('#price');
+var formOfferType = adForm.querySelector('#type');
+var formOfferCheckin = adForm.querySelector('#timein');
+var formOfferCheckout = adForm.querySelector('#timeout');
+var formOfferRooms = adForm.querySelector('#room_number');
+var formOfferGuests = adForm.querySelector('#capacity');
+
+var InputProps = {
+  TITLE: {
+    MIN_LENGTH: 30,
+    MAX_LENGTH: 100
+  },
+  PRICE: {
+    MAX: 1000000,
+    MIN: {
+      BUNGALO: 0,
+      FLAT: 1000,
+      HOUSE: 5000,
+      PALACE: 10000
+    }
+  },
+  CAPACITY: {
+    1: [1],
+    2: [2, 1],
+    3: [3, 2, 1],
+    100: [0],
+    NO_GUESTS_ALLOWED: 100
+  }
+};
+
+var ErrorMessage = {
+  TITLE: {
+    VALUE_MISSING: 'Введите заголовок',
+    TOO_SHORT: 'Заголовок должен состоять минимум из ' + InputProps.TITLE.MIN_LENGTH + ' символов',
+    TOO_LONG: 'Заголовок не может составлять больше ' + InputProps.TITLE.MAX_LENGTH + ' символов',
+  },
+  PRICE: {
+    UNDERFLOW: 'Минимальная цена на ваш вид жилья - ',
+    OVERFLOW: 'Максимальная цена на жильё - ' + InputProps.PRICE.MAX,
+    VALUE_MISSING: 'Укажите цену'
+  },
+  CAPACITY: {
+    MIN: 'Слишком много гостей на количество комнат',
+    MAX: 'Слишком много комнат для гостей'
+  }
+};
+
+function formOfferTitleInputHandler() {
+  if (formOfferTitle.validity.valueMissing) {
+    formOfferTitle.setCustomValidity(ErrorMessage.TITLE.VALUE_MISSING);
+  } else if (formOfferTitle.validity.tooShort) {
+    formOfferTitle.setCustomValidity(ErrorMessage.TITLE.TOO_SHORT);
+  } else if (formOfferTitle.validity.tooLong) {
+    formOfferTitle.setCustomValidity(ErrorMessage.TITLE.TOO_LONG);
+  } else {
+    formOfferTitle.setCustomValidity('');
+  }
+}
+
+function formOfferTypeChangeHandler() {
+  var minPrice = InputProps.PRICE.MIN[formOfferType.value.toUpperCase()];
+  formOfferPrice.placeholder = minPrice;
+  formOfferPrice.min = minPrice;
+}
+
+function formOfferPriceChangeHandler() {
+  if (formOfferPrice.validity.rangeOverflow) {
+    formOfferPrice.setCustomValidity(ErrorMessage.PRICE.OVERFLOW);
+  } else if (formOfferPrice.validity.rangeUnderflow) {
+    formOfferPrice.setCustomValidity(ErrorMessage.PRICE.UNDERFLOW + formOfferPrice.min);
+  } else if (formOfferPrice.validity.valueMissing) {
+    formOfferPrice.setCustomValidity(ErrorMessage.PRICE.VALUE_MISSING);
+  } else {
+    formOfferPrice.setCustomValidity('');
+  }
+}
+
+function formOfferCheckinChangeHandler() {
+  formOfferCheckout.value = formOfferCheckin.value;
+}
+
+function formOfferCheckoutChangeHandler() {
+  formOfferCheckin.value = formOfferCheckout.value;
+}
+
+function formOfferRoomsChangeHandler() {
+  var selectedRooms = +formOfferRooms.value;
+
+  formOfferGuests.value = selectedRooms;
+
+  if (selectedRooms === InputProps.CAPACITY.NO_GUESTS_ALLOWED) {
+    formOfferGuests.value = 0;
+  }
+
+  checkRoomsToGuestValidity();
+}
+
+function formOfferGuestsChangeHandler() {
+  checkRoomsToGuestValidity();
+}
+
+function checkRoomsToGuestValidity() {
+  var selectedRooms = +formOfferRooms.value;
+  var allowedGuests = InputProps.CAPACITY[selectedRooms];
+  var selectedGuests = +formOfferGuests.value;
+
+  if (!allowedGuests.includes(selectedGuests) && !allowedGuests.includes(0)) {
+    formOfferGuests.setCustomValidity(ErrorMessage.CAPACITY.MIN);
+  } else if (!allowedGuests.includes(selectedGuests)) {
+    formOfferGuests.setCustomValidity(ErrorMessage.CAPACITY.MAX);
+  } else {
+    formOfferGuests.setCustomValidity('');
+  }
+}
+
+function addFormOfferHandlers() {
+  formOfferTitle.addEventListener('input', formOfferTitleInputHandler);
+  formOfferType.addEventListener('change', formOfferTypeChangeHandler);
+  formOfferPrice.addEventListener('change', formOfferPriceChangeHandler);
+  formOfferCheckin.addEventListener('change', formOfferCheckinChangeHandler);
+  formOfferCheckout.addEventListener('change', formOfferCheckoutChangeHandler);
+  formOfferRooms.addEventListener('change', formOfferRoomsChangeHandler);
+  formOfferGuests.addEventListener('change', formOfferGuestsChangeHandler);
+}
+
+function removeFormOfferHandlers() {
+  formOfferTitle.removeEventListener('input', formOfferTitleInputHandler);
+  formOfferType.removeEventListener('change', formOfferTypeChangeHandler);
+  formOfferPrice.removeEventListener('change', formOfferPriceChangeHandler);
+  formOfferCheckin.removeEventListener('change', formOfferCheckinChangeHandler);
+  formOfferCheckout.removeEventListener('change', formOfferCheckoutChangeHandler);
+  formOfferRooms.removeEventListener('change', formOfferRoomsChangeHandler);
+  formOfferGuests.addEventListener('change', formOfferGuestsChangeHandler);
 }
 
 // ========================== //
@@ -485,6 +627,7 @@ function mainPinEnterPressHandler(evt) {
 // Обработчик сброса формы
 function formResetClickHandler(evt) {
   evt.preventDefault();
+  adForm.reset();
   disableMap();
 }
 
