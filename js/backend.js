d43e2dc1c56ee337257c7da4baab15cf0eff804c
Rostyslav Miniukov <thresh1337@gmail.com>
2021-09-03 14:30:03 +0300

    Updating quotes due to eslint

--- js/backend.js
+++ js/backend.js
@@ -1,40 +1,40 @@
-"use strict";
+'use strict';
 
 (function () {
   // Адреса сервера
-  var URL_SAVE = "https://21.javascript.pages.academy/keksobooking";
-  var URL_LOAD = URL_SAVE + "/data";
+  var URL_SAVE = 'https://21.javascript.pages.academy/keksobooking';
+  var URL_LOAD = URL_SAVE + '/data';
 
   // Создание запроса
   function addXHR(timeout, onLoad, onError) {
     var xhr = new XMLHttpRequest();
 
     xhr.timeout = timeout || 10000;
-    xhr.responseType = "json";
+    xhr.responseType = 'json';
 
-    xhr.addEventListener("load", function () {
+    xhr.addEventListener('load', function () {
       switch (xhr.status) {
         case 200:
           onLoad(xhr.response);
           break;
         case 400:
-          onError("Неверный запрос");
+          onError('Неверный запрос');
           break;
         case 401:
-          onError("Пользователь не авторизован");
+          onError('Пользователь не авторизован');
           break;
         case 404:
-          onError("Ничего не найдено");
+          onError('Ничего не найдено');
           break;
         default:
-          onError("Неизвестный статус: " + xhr.status + " " + xhr.statusText);
+          onError('Неизвестный статус: ' + xhr.status + ' ' + xhr.statusText);
       }
     });
-    xhr.addEventListener("error", function () {
-      onError("Произошла ошибка соединения");
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
     });
-    xhr.addEventListener("timeout", function () {
-      onError("Запрос не успел выполниться за " + xhr.timeout + "мс");
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
     });
 
     return xhr;
@@ -44,7 +44,7 @@
   function load(onLoad, onError) {
     var xhr = addXHR(10000, onLoad, onError);
 
-    xhr.open("GET", URL_LOAD);
+    xhr.open('GET', URL_LOAD);
     xhr.send();
   }
 
@@ -52,7 +52,7 @@
   function save(data, onLoad, onError) {
     var xhr = addXHR(1000, onLoad, onError);
 
-    xhr.open("POST", URL_SAVE);
+    xhr.open('POST', URL_SAVE);
     xhr.send(data);
   }
 
